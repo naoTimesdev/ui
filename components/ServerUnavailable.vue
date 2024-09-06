d50b8b5ed7ae8c0a046a720ea88314af80694021
noaione <noaione0809@gmail.com>
2024-09-06 18:56:39 +0700

    feat: mouse animation on server unavailable
    simulate how it looks on Nuxt initial load pages

--- components/ServerUnavailable.vue
+++ components/ServerUnavailable.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="flex h-screen flex-col items-center justify-center px-2">
-    <Icon name="i-heroicons-server-stack" class="size-16 animate-pulse p-4" />
-    <h1 class="font-variable mt-4 text-center text-3xl variation-weight-bold">
-      {{ $t("app.unavailable.header") }}
-    </h1>
-    <p class="mt-1 text-center">
-      {{ $t("app.unavailable.message") }}
-    </p>
+    <!-- Main "Screen" -->
+    <div ref="mouseArea" class="absolute left-0 top-0 -z-10 h-screen w-screen overflow-hidden">
+      <!-- This will follow mouse cursor -->
+      <div ref="mouseTrail" class="absolute rounded-full bg-red-300 dark:bg-red-700" />
+    </div>
+    <div class="flex flex-col items-center justify-center">
+      <Icon name="i-heroicons-server-stack" class="size-16 animate-pulse p-4" />
+      <h1 class="font-variable mt-4 text-center text-3xl variation-weight-bold">
+        {{ $t("app.unavailable.header") }}
+      </h1>
+      <p class="mt-1 text-center">
+        {{ $t("app.unavailable.message") }}
+      </p>
+    </div>
   </div>
 </template>
 
@@ -14,6 +21,14 @@
 const timeout = ref<number | NodeJS.Timeout>();
 const retry = ref<number>(0);
 const { pingServer } = useServerStatus();
+
+const mouseArea = ref<HTMLElement | null>(null);
+const mouseTrail = ref<HTMLElement | null>(null);
+const { x, y } = useMouse({ touch: false });
+const { pressed } = useMousePressed({
+  touch: false,
+  drag: false,
+});
 
 function callback() {
   pingServer()
@@ -45,12 +60,102 @@ function backoff() {
   timeout.value = setTimeout(callback, 1000 * backoff);
 }
 
+const unwatchMouseTrail = watch(
+  [x, y],
+  () => {
+    if (mouseTrail.value) {
+      // Set size, the more "center" the bigger
+      // Range from 40px to 300px and bigger blur the more close to center
+      // Drops are linear
+
+      // Center is a boxed area
+      const centerX = window.innerWidth / 2;
+      const centerY = window.innerHeight / 2;
+
+      const distanceX = Math.abs(centerX - x.value);
+      const distanceY = Math.abs(centerY - y.value);
+
+      const maxDistance = Math.sqrt(centerX ** 2 + centerY ** 2);
+      const currentDistance = Math.sqrt(distanceX ** 2 + distanceY ** 2);
+
+      const size = 40 + (1 - currentDistance / maxDistance) * 260; // Size ranges from 40px to 300px
+
+      const mouseX = x.value - size / 2;
+      const mouseY = y.value - size / 2;
+
+      mouseTrail.value.style.left = `${mouseX}px`;
+      mouseTrail.value.style.top = `${mouseY}px`;
+
+      mouseTrail.value.style.width = `${size}px`;
+      mouseTrail.value.style.height = `${size}px`;
+
+      const blurSize = Math.ceil(size / 3);
+
+      mouseTrail.value.style.filter = `blur(${blurSize}px)`;
+    }
+  },
+  {
+    immediate: true,
+  }
+);
+
+const unwatchMouseClick = watch(pressed, (mClick) => {
+  console.log("Click", x.value, y.value);
+
+  if (mClick && mouseArea.value) {
+    const posX = x.value;
+    const posY = y.value;
+
+    const drop = document.createElement("div");
+
+    drop.classList.add("absolute", "rounded-full", "bg-red-300", "dark:bg-red-700");
+
+    const size = (40 + Math.random() * 60) * 2;
+
+    drop.style.width = `${size}px`;
+    drop.style.height = `${size}px`;
+
+    drop.style.left = `${posX - size / 2}px`;
+    drop.style.top = `${posY - size / 2}px`;
+
+    drop.style.filter = `blur(${Math.ceil(size / 3)}px)`;
+
+    mouseArea.value.appendChild(drop);
+
+    // Add shrink animation
+    drop.classList.add("animate-shrink");
+
+    setTimeout(() => {
+      drop.remove();
+    }, 1600);
+  }
+});
+
 onMounted(() => {
   // Wait 1s
   setTimeout(callback, 1000);
 });
 
 onBeforeUnmount(() => {
+  unwatchMouseTrail();
+  unwatchMouseClick();
   clearTimeout(timeout.value);
 });
 </script>
+
+<style lang="postcss">
+.animate-shrink {
+  animation: shrink 1.5s forwards;
+}
+
+@keyframes shrink {
+  0% {
+    transform: scale(1);
+    opacity: 1;
+  }
+  100% {
+    transform: scale(0);
+    opacity: 0;
+  }
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -75,7 +75,12 @@ function getWsEndpoint(originalUrl: string) {
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
-  devtools: { enabled: true },
+  devtools: {
+    enabled: true,
+    timeline: {
+      enabled: true,
+    },
+  },
   modules: [
     "@nuxtjs/tailwindcss",
     "@nuxtjs/color-mode",
